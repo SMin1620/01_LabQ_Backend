492f9de545288924e4f8be3caabcc8b2c468b50f
jay <jaewhan232@naver.com>
2022-06-30 11:29:43 +0900

    Feat - 하수구 수위 api 호출을 위한 class 구현 DrainPipeMonitoringAPI - 하수구 수위 open api 호출하여 데이터 타입 변환 api_settings/DrainPipeMonitoringAPISettings - 하수구 수위 open api 호출시 필요한 인자

---
+++ api/sewer_pipe.py
@@ -0,0 +1,34 @@
+import requests, json
+import datetime
+from .api_settings import DrainPipeMonitoringAPISetting
+
+
+
+
+class  DrainPipeMonitoringAPI:
+    KEY = DrainPipeMonitoringAPISetting.KEY
+    SERVICE = DrainPipeMonitoringAPISetting.SERVICE
+    TYPE = DrainPipeMonitoringAPISetting.TYPE
+    URL = DrainPipeMonitoringAPISetting.URL
+    START_INDEX = DrainPipeMonitoringAPISetting.START_INDEX
+    END_INDEX = DrainPipeMonitoringAPISetting.END_INDEX
+
+    def getJsonData(GUBN=None):
+        now_datetime = datetime.datetime.now()
+        before_datetime = (now_datetime - datetime.timedelta(minutes=20)).strftime('%Y%m%d%H')
+        now_datetime = now_datetime.strftime('%Y%m%d%H')
+        
+        url = DrainPipeMonitoringAPI.URL.format(
+            DrainPipeMonitoringAPI.KEY,
+            DrainPipeMonitoringAPI.TYPE,
+            DrainPipeMonitoringAPI.SERVICE,
+            DrainPipeMonitoringAPI.START_INDEX,
+            DrainPipeMonitoringAPI.END_INDEX,
+            str(GUBN).zfill(2),
+            before_datetime,
+            now_datetime
+            )
+        
+        res = requests.get(url)
+        data = json.loads(res.text)
+        return list(data['DrainpipeMonitoringInfo']['row'][::-1])
