1e7f540436d4460564df9609cc973b4fb6f236ba
jay <jaewhan232@naver.com>
2022-06-30 23:18:03 +0900

    Feat - 하수관로 수위, 강수량 데이터  반환 api 요청 날짜 기준으로 가장 최근 측정 데이터 반환

--- api/views.py
+++ api/views.py
@@ -11,8 +11,12 @@ class Test(View):
         drain_pipe_data = DrainPipeMonitoringAPI.getJsonData(gubn)
         GU_NAME = drain_pipe_data[0]['GUBN_NAM'] + '구'
         rainfall_data = ListRainfallServiceAPI.get_rainfall_data(GU_NAME)
-
+        
+        latest_drain_pipie_data = list(filter(lambda x: x['MEA_YMD'] == drain_pipe_data[0]['MEA_YMD'], drain_pipe_data))
+        latest_rainfall_data = list(filter(lambda x: x['RECEIVE_TIME'] == rainfall_data[0]['RECEIVE_TIME'], rainfall_data))
+        
         if rainfall_data:
-            return JsonResponse({"하수구" : drain_pipe_data, "강우량":rainfall_data})
+            return JsonResponse({"하수구" : latest_drain_pipie_data, "강우량":latest_rainfall_data})
         else:
-            return JsonResponse({"하수구": drain_pipe_data})
+            return JsonResponse({"하수구": latest_drain_pipie_data})
+    
