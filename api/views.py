7e401f5512d017e09f9198d3d6458831379c872a
jay <jaewhan232@naver.com>
2022-06-30 11:31:06 +0900

    Feat - test view에서 하수구 수위 데이터 가져오기 DrainpipeMonitoringAPI 클래스의 getJsonData 메소드로 하수구 수위 데이터 가져오기

--- api/views.py
+++ api/views.py
@@ -7,3 +7,8 @@ from .sewer_pipe import *
 
 # Create your views here.
 
+
+def test(request, gubn):
+    drain_pipe_data = DrainPipeMonitoringAPI.getJsonData(gubn)
+    GU_NAME = drain_pipe_data[0]['GUBN_NAM'] + '구'
+    return JsonResponse({"하수구" : drain_pipe_data})
