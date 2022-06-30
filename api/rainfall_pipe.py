b2a2b054ce308753197076040dbb0a2f6ce70024
Yong Su Jeong <knarrblue@gmail.com>
2022-06-30 12:19:27 +0900

    서울시 강우량 조회 api로 데이터 가져오기

---
+++ api/rainfall_pipe.py
@@ -0,0 +1,13 @@
+import requests
+from .api_settings import DrainPipeMonitoringAPISetting
+
+
+class ListRainfallServiceAPI:
+    KEY = DrainPipeMonitoringAPISetting.KEY
+
+    def get_rainfall_data(GU_NAME=None):
+        if GU_NAME:
+            url = f"http://openapi.seoul.go.kr:8088/{ListRainfallServiceAPI.KEY}/json/ListRainfallService/1/20/{GU_NAME}"
+            data = requests.get(url).json()
+            return list(data['ListRainfallService']['row'])
+        return False
