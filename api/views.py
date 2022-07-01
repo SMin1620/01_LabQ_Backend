df2a1a827320ddea07c0a85007a906ec86cf3a3e
jay <jaewhan232@naver.com>
2022-07-01 11:56:32 +0900

    Feat - rainfall-drain api 예외 처리 GUBN query parameter 없을 경우 404 Resonse 반환 open api 호출에서 예외 발생시 404 Response 반환

--- api/views.py
+++ api/views.py
@@ -8,15 +8,31 @@ from .rainfall_pipe import *
 # Create your views here.
 class RainfallDrainAPI(View):
     def get(self, request):
-        GUBN = request.GET['gubn']
-        if GUBN:
+        try:
+            GUBN = request.GET['gubn']
             drain_pipe_data = DrainPipeMonitoringAPI.getJsonData(GUBN)
             GU_NAME = drain_pipe_data[0]['GUBN_NAM'] + '구'
             rainfall_data = ListRainfallServiceAPI.get_rainfall_data(GU_NAME)
 
-            latest_drain_pipie_data = list(filter(lambda x: x['MEA_YMD'] == drain_pipe_data[0]['MEA_YMD'], drain_pipe_data))
+            latest_drain_pipe_data = list(filter(lambda x: x['MEA_YMD'] == drain_pipe_data[0]['MEA_YMD'], drain_pipe_data))
             latest_rainfall_data = list(filter(lambda x: x['RECEIVE_TIME'] == rainfall_data[0]['RECEIVE_TIME'], rainfall_data))
 
-            return JsonResponse({"하수구" : latest_drain_pipie_data, "강우량":latest_rainfall_data})
-        else:
+            res = {
+                'REQUEST_TIME': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                'GUBN': GUBN,
+                'GU_NAME': GU_NAME,
+                'RAINFALL_DATA': {
+                    'DATA_NUM': len(latest_rainfall_data),
+                    'ROW': latest_rainfall_data
+                },
+                'DRAINPIPE_DATA': {
+                    'DATA_NUM': len(latest_drain_pipe_data),
+                    'ROW': latest_drain_pipe_data
+                }
+            }
+            return JsonResponse(res)
+        
+        except KeyError:
+            return JsonResponse({'status_code' : 404, 'detail' : '잘못된 요청입니다,'}, status=404)
+        except Exception:
             return JsonResponse({'status_code' : 404, 'detail' : '잘못된 요청입니다,'}, status=404)
